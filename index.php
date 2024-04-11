9909c13aee60d19de8e10c535ef8010de486fbc0
Mason Sain <sainmason012@gmail.com>
2024-04-11 13:17:32 -0400

    Added diner image

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Diner app</h1>';
+    //echo '<h1>Diner app</h1>';
 
     // Render a view page
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home-page.html');
 });
 
 // Run Fat-Free
