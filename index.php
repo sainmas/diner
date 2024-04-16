8d0af43361920c567886f53c898e0a7f3b9ae88c
Mason Sain <sainmason012@gmail.com>
2024-04-16 13:50:28 -0400

    Define order1 route

--- index.php
+++ index.php
@@ -48,5 +48,36 @@ $f3->route('GET /menu/dinner', function() {
     echo $view->render('views/dinner-menu.html');
 });
 
+// Order1
+$f3->route('GET|POST /order1', function($f3) {
+    //echo '<h1>Dinner Menu!</h1>';
+
+    //If the page has been POSTed
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        //Get data from post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // if valid data
+        if (true) {
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+        }
+
+    }
+
+    //Render the view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+// Order2
+$f3->route('GET|POST /order2/', function() {
+    //Render the view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
+});
+
 // Run Fat-Free
 $f3->run();
