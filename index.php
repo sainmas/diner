de863bbda224e56fdc8583b643eae97c3667db8d
Mason Sain <sainmason012@gmail.com>
2024-04-16 13:06:34 -0400

    Added menu view pages

--- index.php
+++ index.php
@@ -21,5 +21,32 @@ $f3->route('GET /', function() {
     echo $view->render('views/home-page.html');
 });
 
+// Breakfast menu
+$f3->route('GET /menu/breakfast', function() {
+    //echo '<h1>Breakfast Menu!</h1>';
+
+    //Render the view page
+    $view = new Template();
+    echo $view->render('views/breakfast-menu.html');
+});
+
+// Lunch menu
+$f3->route('GET /menu/lunch', function() {
+    //echo '<h1>Lunch Menu!</h1>';
+
+    //Render the view page
+    $view = new Template();
+    echo $view->render('views/lunch-menu.html');
+});
+
+// Dinner menu
+$f3->route('GET /menu/dinner', function() {
+    //echo '<h1>Dinner Menu!</h1>';
+
+    //Render the view page
+    $view = new Template();
+    echo $view->render('views/dinner-menu.html');
+});
+
 // Run Fat-Free
 $f3->run();
